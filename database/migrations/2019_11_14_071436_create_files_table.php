2203bba8019403e9169468fd444cfd21f45181a0
pavlo poberezhnyi <poberezhnyi@hotmail.com>
2019-11-14 09:16:20 +0200

    create db

---
+++ database/migrations/2019_11_14_071436_create_files_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('files', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('path');
+            $table->text('comments')->nullable();
+            $table->string('url')->nullable();
+            $table->date('delete_after');
+            $table->integer('view_count')->default(0);
+            $table->bigInteger('user_id')->unsigned();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('files');
+    }
+}
